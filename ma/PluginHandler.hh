90a42cc0373c45c1e48778a5d379c4d04fb032d2
Michael Jones <m.pricejones@gmail.com>
2012-09-23 15:38:28 +1200

    Add PluginHandler from maya-plugin-handler repository
    We switch it to be in the "ma" namespace and update the include guards
    too.
    
    It looks pretty incomplete so it'll need to be padded out a bit.

---
+++ ma/PluginHandler.hh
@@ -0,0 +1,252 @@
+#ifndef MA_PLUGINHANDLER_HH
+#define MA_PLUGINHANDLER_HH
+
+#include <maya/MFnPlugin.h>
+
+namespace ma {
+
+class PluginHandler 
+{
+public:
+
+    virtual ~PluginHandler() {}
+
+    virtual MStatus handleNode(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MInitializeFunction initFunction,
+            MPxNode::Type type = MPxNode::kDependNode,
+            const MString* classification = NULL
+            ) = 0;
+
+    virtual MStatus handleShape(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MInitializeFunction initFunction,
+            MCreatorFunction uiCreatorFunction,
+            const MString* classification = NULL
+            ) = 0;
+
+    virtual MStatus handleTransform(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MInitializeFunction initFunction,
+            MCreateXformMatrixFunction xformCreatorFunction,
+            const MTypeId& xformId,
+            const MString* classification = NULL
+            ) = 0;
+
+    virtual MStatus handleCommand(
+            const MString& commandName,
+            MCreatorFunction creatorFunction,
+            MCreateSyntaxFunction createSyntaxFunction = NULL
+            ) = 0;
+
+    virtual MStatus handleData(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MPxData::Type type = MPxData::kData
+            ) = 0;
+
+    virtual MStatus handleControlCommand(
+            const MString& commandName,
+            MCreatorFunction creatorFunction
+            ) = 0;
+};
+
+class InitializePluginHandler : public PluginHandler
+{
+public:
+
+    InitializePluginHandler( MFnPlugin& pluginFn )
+     : m_pluginFn( pluginFn ) {}
+
+    MStatus handleNode(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MInitializeFunction initFunction,
+            MPxNode::Type type = MPxNode::kDependNode,
+            const MString* classification = NULL
+            )
+    {
+        return m_pluginFn.registerNode(
+                typeName,
+                typeId,
+                creatorFunction,
+                initFunction,
+                type,
+                classification
+                );
+    }
+
+    MStatus handleShape(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MInitializeFunction initFunction,
+            MCreatorFunction uiCreatorFunction,
+            const MString* classification = NULL
+            )
+    {
+        return m_pluginFn.registerShape(
+                typeName,
+                typeId,
+                creatorFunction,
+                initFunction,
+                uiCreatorFunction,
+                classification
+                );
+    }
+
+    MStatus handleTransform(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MInitializeFunction initFunction,
+            MCreateXformMatrixFunction xformCreatorFunction,
+            const MTypeId& xformId,
+            const MString* classification = NULL
+            )
+    {
+        return m_pluginFn.registerTransform(
+                typeName,
+                typeId,
+                creatorFunction,
+                initFunction,
+                xformCreatorFunction,
+                xformId,
+                classification
+                );
+    }
+
+    MStatus handleCommand(
+            const MString& commandName,
+            MCreatorFunction creatorFunction,
+            MCreateSyntaxFunction createSyntaxFunction = NULL
+            )
+    {
+        return m_pluginFn.registerCommand(
+                commandName,
+                creatorFunction,
+                createSyntaxFunction
+                );
+    }
+
+    MStatus handleData(
+            const MString& typeName,
+            const MTypeId& typeId,
+            MCreatorFunction creatorFunction,
+            MPxData::Type type = MPxData::kData
+            )
+    {
+        return m_pluginFn.registerData(
+                typeName,
+                typeId,
+                creatorFunction,
+                type
+                );
+    }
+
+    MStatus handleControlCommand(
+            const MString& commandName,
+            MCreatorFunction creatorFunction
+            )
+    {
+        return m_pluginFn.registerControlCommand(
+                commandName,
+                creatorFunction
+                );
+    }
+
+private:
+
+    MFnPlugin& m_pluginFn;
+};
+
+
+class UninitializePluginHandler : public PluginHandler
+{
+public:
+
+    UninitializePluginHandler( MFnPlugin& pluginFn )
+     : m_pluginFn( pluginFn ) {}
+
+    MStatus handleNode(
+            const MString&,
+            const MTypeId& typeId,
+            MCreatorFunction,
+            MInitializeFunction,
+            MPxNode::Type = MPxNode::kDependNode,
+            const MString* = NULL
+            )
+    {
+        return m_pluginFn.deregisterNode( typeId );
+    }
+
+    MStatus handleShape(
+            const MString&,
+            const MTypeId& typeId,
+            MCreatorFunction,
+            MInitializeFunction,
+            MCreatorFunction,
+            const MString* = NULL
+            )
+    {
+        return m_pluginFn.deregisterNode( typeId );
+    }
+
+    MStatus handleTransform(
+            const MString&,
+            const MTypeId& typeId,
+            MCreatorFunction,
+            MInitializeFunction,
+            MCreateXformMatrixFunction,
+            const MTypeId&,
+            const MString* = NULL
+            )
+    {
+        return m_pluginFn.deregisterNode( typeId );
+    }
+
+    MStatus handleCommand(
+            const MString& commandName,
+            MCreatorFunction,
+            MCreateSyntaxFunction = NULL
+            )
+    {
+        return m_pluginFn.deregisterCommand( commandName );
+    }
+
+    MStatus handleData(
+            const MString&,
+            const MTypeId& typeId,
+            MCreatorFunction,
+            MPxData::Type = MPxData::kData
+            )
+    {
+        return m_pluginFn.deregisterData( typeId );
+    }
+
+    MStatus handleControlCommand(
+            const MString& commandName,
+            MCreatorFunction
+            )
+    {
+        return m_pluginFn.deregisterControlCommand( commandName );
+    }
+
+private:
+
+    MFnPlugin& m_pluginFn;
+
+};
+
+}; // end namespace ma
+
+#endif // MA_PLUGINHANDLER_HH
+
